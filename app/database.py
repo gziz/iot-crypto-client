74fd96d936a8191d877cdaa7759525589677a61c
gziz <69287011+gziz@users.noreply.github.com>
2023-05-20 16:24:39 -0600

    Modify db path

--- app/database.py
+++ app/database.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-engine = create_engine("sqlite:///database.db")
+engine = create_engine(os.getenv("CLIENT_DB_URL"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
