0a2e7d1795413dade063f72b27d7d7b092ab4ff6
gziz <69287011+gziz@users.noreply.github.com>
2023-05-20 18:45:38 -0600

    Remove repository file for device endpoints

--- app/routers/device.py
+++ app/routers/device.py
@@ -1,29 +1,49 @@
 import yaml
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, HTTPException
 from sqlalchemy.orm import Session
-from ..repository import device
 from ..database import get_db
 from .. import schemas, database, models
 
 router = APIRouter(prefix = "/device", tags = ["Sensor Data"])
 
-@router.get("/{key}")
-async def get_value(key: str, db: Session = Depends(get_db)):
-    return device.get_value(key, db)
+
+@router.get("/value")
+async def get_value(key: str = Query(...), db: Session = Depends(get_db)):
+    key_value = db.query(models.KeyValue).filter(models.KeyValue.key == key).first()
+    if key_value:
+        return {"value": key_value.value}
+    else:
+        raise HTTPException(status_code=404, detail=f"Key not found")
 
 
-@router.get("/")
+@router.get("/value/all")
 async def get_all(db: Session = Depends(get_db)):
-    return device.get_all(db)
+    return db.query(models.KeyValue).all()
 
 
-@router.post("/")
+@router.post("/value")
 async def insert_value(req: schemas.KeyValue, db: Session = Depends(get_db)):
-    return device.insert_value(req, db)
+    key_value = db.query(models.KeyValue).filter(models.KeyValue.key == req.key).first()
+    if key_value:
+        key_value.value = req.value
+    else:
+        key_value = models.KeyValue(key=req.key, value=req.value)
+        db.add(key_value)
+    db.commit()
+    return {"status": "success"}
 
-@router.put("/")
+
+@router.put("/value")
 async def update_value(req: schemas.KeyValue, db: Session = Depends(get_db)):
-    return device.update_value(req, db)
+    key_value = db.query(models.KeyValue).filter(models.KeyValue.key == req.key).first()
+    if not key_value:
+        raise HTTPException(status_code=404, detail="Key not found")
+
+    key_value.value = req.value
+    db.add(key_value)
+    db.commit()
+    db.refresh(key_value)
+    return key_value
 
 
 @router.get('/init-db')
@@ -32,8 +52,7 @@ def init_db(db: Session = Depends(get_db)):
         with open('app/defaults.yaml', 'r') as f:
             defaults = yaml.safe_load(f)
 
-        defaults = [models.KeyValue(key=key, value=val) for key, val in defaults.items()]
-
+        defaults = [models.KeyValue(key=key, value=value) for key, value in defaults.items()]
         db.add_all(defaults)
         db.commit()
         return {'message': 'Database initialized with default values'}
